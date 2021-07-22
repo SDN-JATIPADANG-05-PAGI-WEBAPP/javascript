e5d2e315735fa5fea37778c44d2731564d270a94
Maskibow <iszroil@gmail.com>
2021-07-22 16:30:12 +0700

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -260,8 +260,8 @@ function resize() {el.style.width = ((el.value.length+1) * int) + 'px'}
 var e = 'keyup,keypress,focus,blur,change'.split(',');
 for (var i in e) el.addEventListener(e[i],resize,false);
 resize();
-}
 resizable(document.getElementById('upload-Preview'),7);
+}
 
 
   function filterFunction() {
